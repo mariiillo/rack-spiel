a3a7def8f5e3a291fa2d0c63aff4702676080a53
Mario Viapiano <mario@webionate.de>
2019-08-28 11:09:53 +0200

    Use Rack::Response abstraction

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require 'rack'
 require 'rack/server'
 
-class HelloWorld
-  def response
-    [ 200, { }, [ 'Hello World' ] ]
-  end
-end
-
 class HelloWorldApp
   def self.call(env)
-    HelloWorld.new.response
+    response = Rack::Response.new
+    response.write 'Hello World 2'      # write some content to the body
+    response.body = [ 'Hello World 2' ] # or set it directly
+    response['X-Custom-Header'] = 'foo'
+    response.set_cookie 'bar', 'baz'
+    response.status = 202
+
+    response.finish # return the generated triplet
   end
 end
 
-Rack::Server.start :app => HelloWorldApp
+Rack::Server.start app: HelloWorldApp
