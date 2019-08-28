d76b97c3f1fdc8e57354b54668055c10c74b2578
Mario Viapiano <mario@webionate.de>
2019-08-28 11:32:37 +0200

    Add and use some custom middleware

--- app.rb
+++ app.rb
@@ -1,15 +1,13 @@
 require 'rack'
 require 'rack/builder'
-require 'rack/etag'
-require 'rack/conditional_get'
-require 'rack/deflater'
 require 'rack/server'
 
+# Class that defines the Rack app
 class HelloWorldApp
-  def self.call(env)
+  def self.call(_env)
     response = Rack::Response.new
-    response.write 'Hello World 2'      # write some content to the body
-    response.body = [ 'Hello World 2' ] # or set it directly
+    response.write 'Hello World 2' # write some content to the body
+    response.body = ['Hello World 2'] # or set it directly
     response['X-Custom-Header'] = 'foo'
     response.set_cookie 'bar', 'baz'
     response.status = 202
@@ -18,10 +16,42 @@ class HelloWorldApp
   end
 end
 
+# Custom middleware that adds the HTTP_ACCEPT Header to the env.
+# This is executed BEFORE HelloWorldApp app
+# The response is sent to HelloWorldApp.
+class EnsureJsonResponse
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    env['HTTP_ACCEPT'] = 'application/json'
+    @app.call(env) # the next middleware is called.
+  end
+end
+
+# Custom middleware that adds the X-Timing Header to the env.
+# This is executed AFTER HelloWorldApp app.
+# The response is sent to the web server.
+class Timer
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    before = Time.now
+    # the next middlewares are called and then
+    # the Timer middleware modifies the env (which is then returned to the user)
+    status, headers, body = @app.call(env)
+
+    headers['X-Timing'] = "#{(Time.now - before) * 1000} sec"
+    [status, headers, body]
+  end
+end
+
 app = Rack::Builder.new do
-  use Rack::ETag
-  use Rack::ConditionalGet
-  use Rack::Deflater
+  use Timer
+  use EnsureJsonResponse
   run HelloWorldApp
 end
 
